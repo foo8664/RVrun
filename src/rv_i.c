f72a8d24a6c9711cfb18e5d44f1309384f8e8e99
Diego Evaristo <di.diegoevaristo@gmail.com>
2026-02-08 11:18:21 -0300

    Removing funct check, MASK & MATCH already do that

--- src/rv_i.c
+++ src/rv_i.c
@@ -6,17 +6,16 @@
 #include "rv_i.h"
 
 static void R_getfields(insn_t insn, enum ABI_REG *rd, enum ABI_REG *rs1,
-			enum ABI_REG *rs2, uint8_t *funct7)
+			enum ABI_REG *rs2)
 	__attribute__((nonnull));
 
 
 static void R_getfields(insn_t insn, enum ABI_REG *rd, enum ABI_REG *rs1,
-			enum ABI_REG *rs2, uint8_t *funct7)
+			enum ABI_REG *rs2)
 {
 	*rd = (enum ABI_REG)((insn & 0xf80) >> 7);
 	*rs1 = (enum ABI_REG)((insn & 0xf8000) >> 15);
 	*rs2 = (enum ABI_REG)((insn & 0xf00000) >> 20);
-	*funct7 = (uint8_t)((insn & 0xff000000) >> 25);
 }
 
 int insn_add(struct proc *proc, insn_t insn)
@@ -24,15 +23,8 @@ int insn_add(struct proc *proc, insn_t insn)
 	enum ABI_REG rs1;
 	enum ABI_REG rs2;
 	enum ABI_REG rd;
-	uint8_t funct7;
 
-	R_getfields(insn, &rd, &rs1, &rs2, &funct7);
-	if (funct7 != 0) {
-		err_log("add: invalid funct7 (0x%hhx)", funct7);
-		errno = ENOSYS;
-		return -1;
-	}
-
+	R_getfields(insn, &rd, &rs1, &rs2);
 	mvreg(proc, rd, getreg(proc, rs1) + getreg(proc, rs2));
 	dbg_log("add: setting x%d to x%d + x%d = %ld", rd, rs1, rs2,
 		getreg(proc, rd));
@@ -44,15 +36,8 @@ int insn_slt(struct proc *proc, insn_t insn)
 	enum ABI_REG rd;
 	enum ABI_REG rs1;
 	enum ABI_REG rs2;
-	uint8_t funct7;
 
-	R_getfields(insn, &rd, &rs1, &rs2, &funct7);
-	if (funct7 != 0) {
-		err_log("slt: invalid funct7 (0x%hhx)", funct7);
-		errno = ENOSYS;
-		return -1;
-	}
-
+	R_getfields(insn, &rd, &rs1, &rs2);
 	mvreg(proc, rd, (ireg_t)getreg(proc, rs1) < (ireg_t)getreg(proc, rs2));
 	dbg_log("stl: Setting x%d to %ld: x%d < x%d?", rd, getreg(proc, rd),
 		rs1, rs2);
@@ -64,15 +49,8 @@ int insn_sltu(struct proc *proc, insn_t insn)
 	enum ABI_REG rd;
 	enum ABI_REG rs1;
 	enum ABI_REG rs2;
-	uint8_t funct7;
 
-	R_getfields(insn, &rd, &rs1, &rs2, &funct7);
-	if (funct7 != 0) {
-		err_log("sltu: invalid funct7 (0x%hhx)", funct7);
-		errno = ENOSYS;
-		return -1;
-	}
-
+	R_getfields(insn, &rd, &rs1, &rs2);
 	mvreg(proc, rd, (ureg_t)getreg(proc, rs1) < (ureg_t)getreg(proc, rs2));
 	dbg_log("stlu: Setting x%d to %ld: x%d < x%d?", rd, getreg(proc, rd),
 		rs1, rs2);
@@ -84,15 +62,8 @@ int insn_and(struct proc *proc, insn_t insn)
 	enum ABI_REG rd;
 	enum ABI_REG rs1;
 	enum ABI_REG rs2;
-	uint8_t funct7;
 
-	R_getfields(insn, &rd, &rs1, &rs2, &funct7);
-	if (funct7 != 0) {
-		err_log("and: invalid funct7 (0x%hhx)", funct7);
-		errno = ENOSYS;
-		return -1;
-	}
-
+	R_getfields(insn, &rd, &rs1, &rs2);
 	mvreg(proc, rd, getreg(proc, rs1) & getreg(proc, rs2));
 	dbg_log("and: Setting x%d = x%d & x%d = 0x%lx", rd, rs1, rs2,
 		getreg(proc, rd));
@@ -104,15 +75,8 @@ int insn_or(struct proc *proc, insn_t insn)
 	enum ABI_REG rd;
 	enum ABI_REG rs1;
 	enum ABI_REG rs2;
-	uint8_t funct7;
 
-	R_getfields(insn, &rd, &rs1, &rs2, &funct7);
-	if (funct7 != 0) {
-		err_log("or: invalid funct7 (0x%hhx)", funct7);
-		errno = ENOSYS;
-		return -1;
-	}
-
+	R_getfields(insn, &rd, &rs1, &rs2);
 	mvreg(proc, rd, getreg(proc, rs1) | getreg(proc, rs2));
 	dbg_log("and: Setting x%d = x%d | x%d = 0x%lx", rd, rs1, rs2,
 		getreg(proc, rd));
@@ -124,15 +88,8 @@ int insn_xor(struct proc *proc, insn_t insn)
 	enum ABI_REG rd;
 	enum ABI_REG rs1;
 	enum ABI_REG rs2;
-	uint8_t funct7;
 
-	R_getfields(insn, &rd, &rs1, &rs2, &funct7);
-	if (funct7 != 0) {
-		err_log("xor: invalid funct7 (0x%hhx)", funct7);
-		errno = ENOSYS;
-		return -1;
-	}
-
+	R_getfields(insn, &rd, &rs1, &rs2);
 	mvreg(proc, rd, getreg(proc, rs1) ^ getreg(proc, rs2));
 	dbg_log("xor: Setting x%d = x%d ^ x%d = 0x%lx", rd, rs1, rs2,
 		getreg(proc, rd));
@@ -144,15 +101,8 @@ int insn_sll(struct proc *proc, insn_t insn)
 	enum ABI_REG rd;
 	enum ABI_REG rs1;
 	enum ABI_REG rs2;
-	uint8_t funct7;
 
-	R_getfields(insn, &rd, &rs1, &rs2, &funct7);
-	if (funct7 != 0) {
-		err_log("sll: invalid funct7 (0x%hhx)", funct7);
-		errno = ENOSYS;
-		return -1;
-	}
-
+	R_getfields(insn, &rd, &rs1, &rs2);
 	mvreg(proc, rd, (reg_t)((ureg_t)getreg(proc, rs1) <<
 				(ureg_t)(getreg(proc, rs2) & 0x3f)));
 	dbg_log("sll: Setting x%d = x%d << x%d = 0x%lx", rd, rs1, rs2,
@@ -165,15 +115,8 @@ int insn_srl(struct proc *proc, insn_t insn)
 	enum ABI_REG rd;
 	enum ABI_REG rs1;
 	enum ABI_REG rs2;
-	uint8_t funct7;
 
-	R_getfields(insn, &rd, &rs1, &rs2, &funct7);
-	if (funct7 != 0) {
-		err_log("srl: invalid funct7 (0x%hhx)", funct7);
-		errno = ENOSYS;
-		return -1;
-	}
-
+	R_getfields(insn, &rd, &rs1, &rs2);
 	mvreg(proc, rd, (reg_t)((ureg_t)getreg(proc, rs1) >>
 				(ureg_t)(getreg(proc, rs2) & 0x3f)));
 	dbg_log("srl: Setting x%d = x%d >> x%d = 0x%lx", rd, rs1, rs2,
@@ -186,15 +129,8 @@ int insn_sra(struct proc *proc, insn_t insn)
 	enum ABI_REG rd;
 	enum ABI_REG rs1;
 	enum ABI_REG rs2;
-	uint8_t funct7;
 
-	R_getfields(insn, &rd, &rs1, &rs2, &funct7);
-	if (funct7 != 0x20) {
-		err_log("sra: invalid funct7 (0x%hhx)", funct7);
-		errno = ENOSYS;
-		return -1;
-	}
-
+	R_getfields(insn, &rd, &rs1, &rs2);
 	mvreg(proc, rd, (reg_t)((ireg_t)getreg(proc, rs1) >>
 				(ireg_t)(getreg(proc, rs2) & 0x3f)));
 	dbg_log("sra: Setting x%d = x%d >> x%d = 0x%lx", rd, rs1, rs2,
@@ -207,15 +143,8 @@ int insn_sub(struct proc *proc, insn_t insn)
 	enum ABI_REG rd;
 	enum ABI_REG rs1;
 	enum ABI_REG rs2;
-	uint8_t funct7;
 
-	R_getfields(insn, &rd, &rs1, &rs2, &funct7);
-	if (funct7 != 0x20) {
-		err_log("sub: invalid funct7 (0x%hhx)", funct7);
-		errno = ENOSYS;
-		return -1;
-	}
-
+	R_getfields(insn, &rd, &rs1, &rs2);
 	mvreg(proc, rd, getreg(proc, rs1) - getreg(proc, rs2));
 	dbg_log("sub: Setting x%d = x%d - x%d = %ld", rd, rs1, rs2,
 		getreg(proc, rd));
